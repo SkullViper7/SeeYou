5826f637936cdc52ae108d180770bd582691e005
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-01-24 11:32:19 +0100

    footsteps variation terrain

---
+++ Assets/Scripts/Audio/FootstepsSFX.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsSFX : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> _grassSFX;
+    [SerializeField]
+    List<AudioClip> _gravelSFX;
+    [SerializeField]
+    List<AudioClip> _mudSFX;
+
+    [SerializeField]
+    AudioSource _audioSource;
+
+    TerrainDetector _terrainDetector;
+
+    private void Awake()
+    {
+        _terrainDetector = new TerrainDetector();
+    }
+
+    void Step()
+    {
+        AudioClip _clip = GetRandomClip();
+        _audioSource.PlayOneShot(_clip);
+    }
+
+    AudioClip GetRandomClip()
+    {
+        int terrainTextureIndex = _terrainDetector.GetActiveTerrainTextureIdx(transform.position);
+
+        switch (terrainTextureIndex)
+        {
+            case 0:
+            default:
+                return _grassSFX[Random.Range(0, _grassSFX.Count)];
+            case 1:
+                return _gravelSFX[Random.Range(0, _gravelSFX.Count)];
+            case 2:
+                return _mudSFX[Random.Range(0, _mudSFX.Count)];
+        }
+    }
+}
