0c242da2ac874341f16d201e3c187e823bcfae98
Aurelien Tavernier <aurelientav@gmail.com>
2024-05-06 09:25:45 +0200

    player changed

--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -29,7 +29,8 @@ public class PlayerInputs : MonoBehaviour
     {
         if (this._eventShoot == null)
         {
-            this._eventShoot += this._playerMain.shoot.ShootingServerRpc;
+            this._eventShoot += this._playerMain.shoot.Shooting;
+            this._eventShoot += this._playerMain.shoot.SyncShoot;
             this._eventShoot += this._playerMain.playerNetwork.RolesChangesServerRpc;
         }
 
