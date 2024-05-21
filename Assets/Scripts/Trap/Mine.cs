b2415f566e27355b29c0fddb2cb3ec5851b3402a
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-05-21 14:17:54 +0200

    mine fix

--- Assets/Scripts/Trap/Mine.cs
+++ Assets/Scripts/Trap/Mine.cs
@@ -14,9 +14,16 @@ public class Mine : MonoBehaviour
 
     ParticleSystem _particleSystem;
 
+    [SerializeField] GameObject _mesh;
+
+    [SerializeField] AudioClip _sfx;
+    AudioSource _audioSource;
+
     private void Awake()
     {
         _particleSystem = GetComponentInChildren<ParticleSystem>();
+
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -38,10 +45,12 @@ public class Mine : MonoBehaviour
     {
         if (other.gameObject.tag == "Prey")
         {
-            //Destroy(collision.gameObject);
+            Destroy(other.gameObject);
             _particleSystem.Play();
             zone.SetActive(true);
             ImpulseManager.Instance.Shake(2, 3, new Vector3(0.25f, 0.25f, 0.25f), 0.5f);
+            _mesh.SetActive(false);
+            _audioSource.PlayOneShot(_sfx);
         }
     }
 }
