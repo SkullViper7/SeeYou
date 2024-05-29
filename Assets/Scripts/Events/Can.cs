f16ff672667cb0bdf2ed06945a89aa2915c4ae28
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-05-29 09:57:39 +0200

    script documentation

--- Assets/Scripts/Events/Can.cs
+++ Assets/Scripts/Events/Can.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Can : MonoBehaviour
@@ -12,21 +9,39 @@ public class Can : MonoBehaviour
 
     [SerializeField] float _force;
 
+    /// <summary>
+    /// This method is called when the script instance is being loaded.
+    /// </summary>
+    /// <remarks>
+    /// This method is called when the script instance is being loaded. Awake is called when the script instance
+    /// is being loaded. This method is called before any Start methods and allows you to initialize variables and
+    /// other data before the game starts.
+    /// </remarks>
     private void Awake()
     {
+        // Get the AudioSource component attached to the game object
         _audioSource = GetComponent<AudioSource>();
 
+        // Get the Rigidbody component attached to the game object
         _rb = GetComponent<Rigidbody>();
     }
 
+    /// <summary>
+    /// This method is called when this collider/rigidbody has begun touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
     void OnTriggerEnter(Collider other)
     {
+        // Generate a random direction for the force to be applied
         Vector3 randomDir = new Vector3(Random.Range(-_force, _force), 0, Random.Range(-_force, _force));
 
+        // Check if the collided object has the tag "Prey"
         if (other.gameObject.tag == "Prey")
         {
+            // Play a random audio clip from the array
             _audioSource.PlayOneShot(_clips[Random.Range(0, _clips.Length)]);
 
+            // Apply a force to the rigidbody of the collided object in the random direction
             _rb.AddForce(randomDir, ForceMode.Impulse);
         }
     }
