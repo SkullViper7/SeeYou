52786301e6c2c1b74f2f74bcad9cdb17e9170caf
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-06-12 14:13:43 +0200

    stuff

--- Assets/Scripts/Menu/Splash/Splash.cs
+++ Assets/Scripts/Menu/Splash/Splash.cs
@@ -1,8 +1,12 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour
 {
+    [SerializeField] GameObject _loadingScreen;
+
     private void Start()
     {
         Invoke("LoadMenu", 4);
@@ -10,6 +14,20 @@ public class Splash : MonoBehaviour
 
     void LoadMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        _loadingScreen.SetActive(true);
+        StartCoroutine(WaitBeforeLoadMenu());
+    }
+
+    IEnumerator WaitBeforeLoadMenu()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("MainMenu");
+
+        asyncOperation.allowSceneActivation = false;
+
+        yield return new WaitUntil(() => DynamicGI.isConverged);
+
+        asyncOperation.allowSceneActivation = true;
+
+        yield return null;
     }
 }
